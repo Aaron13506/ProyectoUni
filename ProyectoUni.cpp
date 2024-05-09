b6858bcc7f8166f8ea4dc40045fb567398fd7a08
Aaron13506 <aronquintana13@gmail.com>
2024-05-09 16:24:09 -0400

    v1.2

--- ProyectoUni.cpp
+++ ProyectoUni.cpp
@@ -10,17 +10,18 @@ using namespace std;
 int main() {
     const double PI = M_PI; //Constante PI
     const double GRAVITY = -9.81; //Constante gravedad
+    const char character164 = static_cast<char>(164);
     int initialPositionX, initialPositionY, initialSpeed, actualAngle, objectiveQty, actualTargetX, actualTargetY, cannonQty;
     double timeInAir; //Luego se calcula cuanto tarda en caer el suelo según la altura inicial(Funcionando)
-    cout << "Ingrese el numero de ca" << static_cast<char>(164) << "ones:" << std::endl;
+    cout << "Ingrese el numero de ca" << character164 << "ones:" << std::endl;
     cin >> cannonQty;
     int canonID = 65;
     char canonLetter = static_cast<char>(canonID);
     for (int i = 0; i < cannonQty; ++i) {
-        cout << "Ingrese la coordenada X y Y del ca" << static_cast<char>(164) << "on separado por un espacio:"
+        cout << "Ingrese la coordenada X y Y del ca" << character164 << "on separado por un espacio:"
              << std::endl;
         cin >> initialPositionX >> initialPositionY;
-        cout << "Ingrese la velocidad y el angulo del ca" << static_cast<char>(164) << "on separado por un espacio:"
+        cout << "Ingrese la velocidad y el angulo del ca" << character164 << "on separado por un espacio:"
              << std::endl;
         cin >> initialSpeed >> actualAngle;
         double senoAngle = sin(actualAngle * PI / 180); //Numero convertido a radianes para calcular seno(Funcionando)
@@ -40,32 +41,39 @@ int main() {
                          sqrt((pow(initialSpeed, 2) * senoAngleSquare + 2 * -GRAVITY * initialPositionY))) /
                         -GRAVITY; //  (Funcionando)
         }
-        cout << "Los proyectiles del ca" << static_cast<char>(164) << "on " << canonLetter << " subiran hasta "
+        cout << "Los proyectiles del ca" << character164 << "on " << canonLetter << " subiran hasta "
              << static_cast<int>(round(maxHeight)) << " metros antes de comenzar a caer." << std::endl;
         cout << "Estos impactaran contra el suelo pasados " << round(timeInAir) << " segundos luego de ser disparados."
              << std::endl;
         cout << "Ingrese el numero de objetivos:" << std::endl;
         cin >> objectiveQty;
         for (int j = 0; j < objectiveQty; ++j) {
+            cout<<"Ingrese la coordenada X y Y del objetivo separadas por un espacio"<<endl;
+            cin>>actualTargetX>>actualTargetY;
+            double timeY = (-initialSpeed * senoAngle + sqrt(
+                    (pow(initialSpeed, 2) * senoAngleSquare + 2 * GRAVITY * (actualTargetY - initialPositionY)))) /
+                           GRAVITY;//(Funcionando)
+            double timeY2 = -(
+                    (initialSpeedY + sqrt(pow(initialSpeedY, 2) + 2 * GRAVITY * (actualTargetY - initialPositionY))) /
+                    GRAVITY);
+            double xWhenY1 = round((initialSpeed * (timeY) * cosAngle) +
+                                   initialPositionX); //Indica donde se encuentra el proyectil en el eje X cuando pasa por el eje Y indicado
+            double xWhenY2 = round((initialSpeed * (timeY2) * cosAngle) +
+                                   initialPositionX);
+            if (xWhenY1 == actualTargetX) {
+                cout << "Objetivo " << j + 1 << " destruido por el ca" << character164 << "on " << canonLetter << " en "
+                     << round(timeY) << " segundos." << endl;
+            } else if (xWhenY2 == actualTargetX) {
+                cout << "Objetivo " << j + 1 << " destruido por el ca" << character164 << "on " << canonLetter << " en "
+                     << round(timeY2) << " segundos." << endl;
+            }
+            if (xWhenY2){}
 
         }
         canonID++;
     }
-    actualTargetX = 850;
-    actualTargetY = 143;
 
 
 
-    /*double timeY = (-initialSpeed * senoAngle + sqrt(
-            (pow(initialSpeed, 2) * senoAngleSquare + 2 * GRAVITY * (actualTargetY - initialPositionY)))) /
-                   GRAVITY;//(Funcionando)
-    double timeY2 = -(
-            (initialSpeedY + sqrt(pow(initialSpeedY, 2) + 2 * GRAVITY * (actualTargetY - initialPositionY))) /
-            GRAVITY);
-
-    double xWhenY = (initialSpeed * (timeY2) * cosAngle) +
-                    initialPositionX; //Indica donde se encuentra el proyectil en el eje X cuando pasa por el eje Y indicado
-
-    std::cout << initialPositionY << " " << initialPositionX << " " << xWhenY;*/
     return 0;
 }
