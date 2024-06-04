ccc74e9769e5df689ddd5157d97b8bb52b76d691
Aaron <aronquintana13@gmail.com>
2024-06-04 08:37:27 -0400

    Trabajo final, falta comentar

--- ProyectoUni.cpp
+++ ProyectoUni.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-
+//Funcion para calcular si le diste al objetivo
 double calcularYcuandoX(double PI, double GRAVITY, int initialSpeed, int initialPositionX, int initialPositionY,
                         int actualTargetX, double newActualAngle) {
     double newSenoAngle = sin(
@@ -26,30 +26,35 @@ double calcularYcuandoX(double PI, double GRAVITY, int initialSpeed, int initial
 
 
 int main() {
-    setlocale(LC_ALL, "spanish");
-    string char164 = "ñ"; //Permite colocar la letra
+    setlocale(LC_ALL, "spanish"); //Permite colocar la letra ñ
+    string char164 = "ñ";
     const double PI = M_PI; //Constante PI
     const double GRAVEDAD = -9.81; //Constante gravedad
     int posicionInicialX, posicionInicialY, velocidadInicial, anguloRecibido, nroObjetivos, xDeObjetivo, yDeObjetivo, nroCanones;
     int canonNumberID = 65;
     double tiempoEnAire; //Luego se calcula cuanto tarda en caer el suelo según la altura inicial(Funcionando)
+    //Entrada de datos
     cin >> nroCanones;
+    //Validacion de datos
     if (nroCanones > 26 || nroCanones < 1) {
         cout << "\nDatos de entrada invalidos\n";
         return 0;
     }
     for (int i = 0; i < nroCanones; i++) {
-        char canonLetterID = static_cast<char>(canonNumberID);
+        char canonLetterID = static_cast<char>(canonNumberID); //Convierte el numero en letra por cada cañon
         string canonStringID(1, canonLetterID);
         canonNumberID++;
+        //Entrada de datos
         cin >> posicionInicialX >> posicionInicialY;
         cin >> velocidadInicial >> anguloRecibido;
+        //Validacion de datos
         if (posicionInicialY > 50 || posicionInicialY < 0
             || velocidadInicial > 500 || velocidadInicial < 1
             || anguloRecibido > 180 || anguloRecibido < 0) {
             cout << "\nDatos de entrada invalidos\n";
             return 0;
         }
+        //Calculos para determinar altura maxima y tiempo de caida
         double senoAngulo = sin(
                 anguloRecibido * PI / 180); //Numero convertido a radianes para calcular seno(Funcionando)
         double cosenoAngulo = cos(
@@ -60,7 +65,8 @@ int main() {
                               posicionInicialY; //Altura maxima Vo²*Sen²(a)/2*g + Altura inicial (Funcionando)
         int alturaRedondeada = static_cast<int>(round(alturaMaxima));
         string stringAltura = to_string(alturaRedondeada);
-        if (posicionInicialY == 0) { //Las fórmulas difieren si la posición inicial de Y es 0 o no
+        //Calcular tiempo en aire, las fórmulas difieren si la posición inicial de Y es 0 o no
+        if (posicionInicialY == 0) {
             tiempoEnAire = ((2 * velocidadInicial) * senoAngulo /
                             -GRAVEDAD); //Tiempo de vuelo si la altura es 0  Vo*Sen(a)/g (Funcionando)
 
@@ -69,6 +75,7 @@ int main() {
                             sqrt((pow(velocidadInicial, 2) * cuadradoSenoAngulo + 2 * -GRAVEDAD * posicionInicialY))) /
                            -GRAVEDAD; //  (Funcionando)
         }
+
         int tiempoEnAireRedondeado = static_cast<int>(round(tiempoEnAire));
         string stringTiempoEnAire = to_string(tiempoEnAireRedondeado);
         cout << ("\nLos proyectiles del ca")
@@ -81,17 +88,22 @@ int main() {
              << ("Estos impactaran contra el suelo pasados ")
              << (stringTiempoEnAire)
              << (" segundos luego de ser disparados.\n");
+        //Entrada de datos
         cin >> nroObjetivos;
-        if(nroObjetivos<0){
+        //Validacion de datos
+        if (nroObjetivos < 0) {
             cout << "\nDatos de entrada invalidos\n";
             return 0;
         }
         for (int j = 0; j < nroObjetivos; ++j) {
+            //Entrada de datos
             cin >> xDeObjetivo >> yDeObjetivo;
+            //Validacion de datos
             if (yDeObjetivo < 0) {
                 cout << "\nDatos de entrada invalidos\n";
                 return 0;
             }
+            //Calcular tiempo en llegar el eje X, determina en cuanto tiempo le pegaste el objetivo
             double tiempoX = (xDeObjetivo - posicionInicialX) / velocidadInicialX;//(Funcionando)
             int tiempoXRedondeado = static_cast<int>(round(tiempoX));
             string stringTiempoX = to_string(tiempoXRedondeado);
@@ -101,17 +113,19 @@ int main() {
                                                xDeObjetivo, anguloRecibido);
             int yCuandoXRedondeado = static_cast<int>(yCuandoX);
             string stringYCuandoX = to_string(yCuandoXRedondeado);
-
+//Caso 1 (cañon destruido)
             if (xDeObjetivo == posicionInicialX && yDeObjetivo == posicionInicialY) {
                 cout << ("\nCa") << (char164) << ("on destruido\n");
                 break;
+//Caso 2 (Enemigos en las murallas)
             } else if (xDeObjetivo == posicionInicialX && yDeObjetivo < posicionInicialY) {
                 cout << ("\nEnemigos en las murallas\n");
 
-
+//Caso 3 (Posicion comprometida)
             } else if ((xDeObjetivo > posicionInicialX && anguloRecibido > 90) ||
                        (xDeObjetivo < posicionInicialX && anguloRecibido < 90)) {
                 cout << ("\nPosicion comprometida\n");
+//Caso 4 (Objetivo destruido)
             } else if (yCuandoX == yDeObjetivo) {
                 cout << ("\nObjetivo ")
                      << (to_string(j + 1))
@@ -122,13 +136,17 @@ int main() {
                      << (" en ")
                      << (stringTiempoX)
                      << (" segundos.\n");
+//Caso 5 (Reajuste de grados)
             } else {
+                //Se consiguen los grados por aproximacion
                 double anguloInicial = 0;
                 if (anguloRecibido > 90) { anguloInicial = 180; }
                 double anguloFinal;
                 double rangoDeComparacion = 10;
                 double anguloFuncional = 0;
+                //Defines dos variables con angulo inicial y final de busqueda, teniendo un rango de comparacion
                 for (int k = 0; k < 80; ++k) {
+                    //Determina si los angulos deben subir o bajar segun el lado del cañon
                     if (anguloRecibido > 90) { anguloFinal = anguloInicial - rangoDeComparacion; }
                     else { anguloFinal = anguloInicial + rangoDeComparacion; }
                     double calculoConAnguloInicial = calcularYcuandoX(PI, GRAVEDAD, velocidadInicial, posicionInicialX,
@@ -138,6 +156,7 @@ int main() {
                                                                     posicionInicialY, xDeObjetivo, anguloFinal);
                     if (calculoConAnguloInicial == yDeObjetivo) { anguloFuncional = round(anguloInicial); }
                     else if (calculoConAnguloFinal == yDeObjetivo) { anguloFuncional = round(anguloFinal); }
+                    //Si consigues los grados necesarios, se calcula el reajuste
                     if (calculoConAnguloInicial == yDeObjetivo || calculoConAnguloFinal == yDeObjetivo) {
                         int movedGrades = static_cast<int>(abs(anguloRecibido - anguloFuncional));
                         string stringGrades = to_string(movedGrades);
@@ -149,8 +168,10 @@ int main() {
                              << (canonStringID)
                              << ("\n");
                         break;
+//Si no lo consigues, compruebas que tus angulos esten en el rango para poder darle al objetivo y disminuir el rango de busqueda
                     } else if (calculoConAnguloInicial < yDeObjetivo && yDeObjetivo < calculoConAnguloFinal) {
                         rangoDeComparacion /= 10;
+//SI no estan en el rango, pasas al siguiente rango de busqueda
                     } else {
                         if (anguloRecibido > 90) { anguloInicial -= rangoDeComparacion; }
                         else { anguloInicial += rangoDeComparacion; }
